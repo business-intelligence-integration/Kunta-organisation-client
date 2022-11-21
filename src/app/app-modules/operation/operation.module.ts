04caab7d6ae020f36a2414b02a183affcc990426
nguepempoue <nguepempouejulio@gmail.com>
2022-11-21 16:55:57 +0100

    v stable du 21/11/2022

--- src/app/app-modules/operation/operation.module.ts
+++ src/app/app-modules/operation/operation.module.ts
@@ -16,7 +16,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     OperationRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
+    ReactiveFormsModule
   ]
 })
 export class OperationModule { }
