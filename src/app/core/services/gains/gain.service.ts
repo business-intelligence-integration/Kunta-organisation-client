1ae1203f258b4b9f86c50a7a2dc83b8f4e677573
nguepempoue <nguepempouejulio@gmail.com>
2022-11-22 04:01:18 +0100

    fin gestion des gains

---
+++ src/app/core/services/gains/gain.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Gain } from '../../classes/gain';
+import { UtilityService } from '../utility/utility.service';
+
+const httpOptions ={
+  headers: new HttpHeaders({
+    'content-type':'application/json'
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GainService {
+
+  private baseUrl = environment.baseUrlApiActivity
+  constructor(private httpClient: HttpClient, private utilityService: UtilityService){
+    httpOptions.headers = httpOptions.headers.set('Authorization', "Bearer " + this.utilityService.loadToken())
+  }
+
+  findAllGainModes():Observable<any>{
+    return this.httpClient.get<any>(this.baseUrl + 'gain-modes', httpOptions);
+  }
+
+  createGainMode(gain: Gain):Observable<any>{
+    return this.httpClient.post<any>(this.baseUrl + 'gain-modes', gain, httpOptions);
+  }
+
+  findGainModeById(id: number):Observable<any>{
+    return this.httpClient.get<any>(this.baseUrl + 'gain-modes/'+ id, httpOptions);
+  }
+
+  updateGainMode(gain: Gain, id: number):Observable<any>{
+    return this.httpClient.put<any>(this.baseUrl + 'gain-modes/'+ id, gain, httpOptions);
+  }
+
+  deleteGainMode(id: number):Observable<any>{
+    return this.httpClient.delete<any>(this.baseUrl + 'gain-modes/'+ id, httpOptions);
+  }
+}
