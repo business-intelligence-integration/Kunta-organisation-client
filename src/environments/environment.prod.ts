d1e3f770a476239baf8c1dcfb3a58a90b948c021
nguepempoue <nguepempouejulio@gmail.com>
2023-01-11 10:03:41 +0100

    version avant le nétoyage de la plate forme

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrlApi: 'http://185.98.139.14:8080/kt-organisation/',
+  baseUrlApiActivity: 'http://185.98.139.14:8080/kt-activities/'
 };
