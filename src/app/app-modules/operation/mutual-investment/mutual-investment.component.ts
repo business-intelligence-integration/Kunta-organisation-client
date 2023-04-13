b28ee895663f6cfcc4a8d3200b7379e6e7be4ec0
LeMatur <mathurintagne@gmail.com>
2023-04-13 09:43:29 +0100

    gestion creation placement

--- src/app/app-modules/operation/mutual-investment/mutual-investment.component.ts
+++ src/app/app-modules/operation/mutual-investment/mutual-investment.component.ts
@@ -9,7 +9,11 @@ import { MutualInvestmentService } from 'src/app/core/services/mutual-investment
 import { ProfitabilityTypeService } from 'src/app/core/services/mutual-investment/profitability-type/profitability-type.service';
 import { RefundTypeService } from 'src/app/core/services/mutual-investment/refund-type/refund-type.service';
 import { UtilityService } from 'src/app/core/services/utility/utility.service';
+import { UserService } from 'src/app/core/services/users/user.service';
 import Swal from 'sweetalert2';
+import { User } from 'src/app/core/classes/user';
+import { FrequencyService } from 'src/app/core/services/frequencies/frequency.service';
+import { Frequency } from 'src/app/core/classes/frequency';
 
 @Component({
   selector: 'app-mutual-investment',
@@ -20,6 +24,8 @@ export class MutualInvestmentComponent implements OnInit {
   ngSelect1 = 0;
   ngSelect2 = 0;
   ngSelect3 = 0;
+  ngSelect4 = 0;
+  ngSelect5 = 0;
   openCreateModal: string = ""
   createMutualInvestmentForm!: FormGroup
   endDate: any;
@@ -32,34 +38,46 @@ export class MutualInvestmentComponent implements OnInit {
   mutualInvestment: MutualInvestment = new MutualInvestment();
   mutualInvestments: MutualInvestment[] = [];
 
+  isPhysical: boolean = false;
+  isOthers: boolean = false;
+  isCertain: boolean = false;
+  isPeriod: boolean = false;
+  users: User[] =[];
+  frequencies: Frequency[] = [];
   openUpdateModal: string = "";
   updateMutualInvestmentForm!: FormGroup;
 
   constructor(private mutualInvestmentService: MutualInvestmentService,
     private draweeFormService: DraweeFormService,
+    private userService: UserService,
     private formBuilder: FormBuilder, 
     private utilityService: UtilityService,
     private profitabilityTypeService: ProfitabilityTypeService,
-    private refundTypeService: RefundTypeService,) { }
+    private refundTypeService: RefundTypeService,
+    private frequencyService: FrequencyService,) { }
 
   ngOnInit(): void {
     this.getAllMutualInvestments();
     this.getAllDroweeForm();
+    this.getAllUsers();
     this.getAllProfitabilityType();
     this.getAllRefundTypes();
+    this.getAllFrequency();
     this.formInit();
   }
 
   formInit() {
     this.createMutualInvestmentForm = this.formBuilder.group({
       name: new FormControl(null, Validators.required),
-      organism: new FormControl(null, Validators.required),
+      organism: new FormControl(null),
       minimumAmount: new FormControl(null, Validators.required),
       idDraweeForm: new FormControl(null, Validators.required),
+      idMutualist: new FormControl(null),
       idProfitabilityType: new FormControl(null, Validators.required),
       idRefundType: new FormControl(null, Validators.required),
-      profitabilityRate: new FormControl(null, Validators.required),
-      echeanceDurationInMonths: new FormControl(null, Validators.required),
+      idFrequency: new FormControl(null),
+      profitabilityRate: new FormControl(null),
+      echeanceDurationInMonths: new FormControl(null),
       rating: new FormControl(null, Validators.required),
       startDate: new FormControl(null, Validators.required),
       endDate: new FormControl(null, Validators.required),
@@ -85,6 +103,12 @@ export class MutualInvestmentComponent implements OnInit {
     })
   }
 
+  getAllUsers() {
+    this.userService.getAllUsers().subscribe((res)=> {
+      this.users = res.data;
+    })
+  }
+
   getAllProfitabilityType(){
     this.profitabilityTypeService.findAllProfitabilityTypes().subscribe((res)=>{
       this.profitabilityTypes = res.data;
@@ -95,6 +119,38 @@ export class MutualInvestmentComponent implements OnInit {
     this.refundTypeService.findAllRefundTypes().subscribe((res)=>{
       this.refundTypes = res.data;
     })
+  }
+
+  getAllFrequency(){
+    this.frequencyService.findAllFrequencies().subscribe((res)=>{
+      this.frequencies = res.data;
+    })
+  }
+
+  onDraweeSelected(val: any) {
+    if(val == 2) {
+      this.isPhysical = true;
+      this.isOthers = false;
+    } else if (val != 2 && val != 0) {
+      this.isPhysical = false;
+      this.isOthers = true;
+    }
+  }
+  
+  onProfitabilitySelected(val: any) {
+    if(val == 1) {
+      this.isCertain = true;
+    } else {
+      this.isCertain = false;
+    }
+  }
+
+  onRefundSelected(val: any) {
+    if (val == 2) {
+      this.isPeriod = true;
+    } else {
+      this.isPeriod = false;
+    }
   }
 
   onOpenCreateModal(){
@@ -116,12 +172,17 @@ export class MutualInvestmentComponent implements OnInit {
     this.mutualInvestment.organism = formValue.organism;
     this.mutualInvestment.profitabilityRate = formValue.profitabilityRate;
     this.mutualInvestment.rating = formValue.rating;
-    this.createAMutualInvestment(this.mutualInvestment, formValue.idDraweeForm, formValue.idRefundType, formValue.idProfitabilityType);
+
+    this.createAMutualInvestment(this.mutualInvestment, formValue.idDraweeForm, formValue.idRefundType, formValue.idProfitabilityType, formValue.idFrequency, formValue.idMutualist);
     
   }
 
-  createAMutualInvestment(mutualInvestment: MutualInvestment, idDraweeForm: number, idRefundType: number, idProfitabilityType: number){
-    this.mutualInvestmentService.createMutualInvestment(mutualInvestment, idDraweeForm, idRefundType, idProfitabilityType).subscribe(()=>{
+  createAMutualInvestment(mutualInvestment: MutualInvestment, idDraweeForm: number, idRefundType: number, idProfitabilityType: number, idFrequency: number, idMutualist: number){
+    console.log('mutualInvestment..', mutualInvestment);
+    console.log('idDraweeForm..', idDraweeForm);
+    console.log('idRefundType..', idRefundType);
+    console.log('idProfitabilityType..', idProfitabilityType);
+    this.mutualInvestmentService.createMutualInvestment(mutualInvestment, idDraweeForm, idRefundType, idProfitabilityType, idFrequency, idMutualist).subscribe(()=>{
       this.isSaving = false;
       this.getAllMutualInvestments();
       this.createMutualInvestmentForm.reset();
@@ -177,14 +238,14 @@ export class MutualInvestmentComponent implements OnInit {
   }
 
   onSubmitUpdateMutualInvestment() {
-    this.isSaving = true;
+    // this.isSaving = true;
     const formValue = this.updateMutualInvestmentForm.value;
     this.mutualInvestment.name = formValue.name;
     this.mutualInvestment.organism = formValue.organism;
     this.mutualInvestment.minimumAmount = formValue.minimumAmount;
     this.mutualInvestment.profitabilityRate = formValue.profitabilityRate;
     this.mutualInvestmentService.updateMutualInvestment(this.mutualInvestment, formValue.id).subscribe(() => {
-      this.isSaving = false;
+      // this.isSaving = false;
       this.getAllMutualInvestments();
       this.onCloseUpdateModal();
       this.utilityService.showMessage(
@@ -194,7 +255,7 @@ export class MutualInvestmentComponent implements OnInit {
         'white'
       );
     },()=>{
-      this.isSaving = false;
+      // this.isSaving = false;
     })
   }
 
