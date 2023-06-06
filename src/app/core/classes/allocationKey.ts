2cef5a23f01a9333bd4aefcd3b587187efa037f1
LeMatur <mathurintagne@gmail.com>
2023-06-06 18:08:11 +0100

    allocation keys

--- src/app/core/classes/allocationKey.ts
+++ src/app/core/classes/allocationKey.ts
@@ -1,5 +1,8 @@
+import { ReceivingParty } from "./receivingParty";
+
 export class AllocationKey{
     id: number = 0;
     percentage: number = 0;
     receivingAmount: number = 0;
+    receivingParty: ReceivingParty = new ReceivingParty();
 }
---
+++ src/app/core/classes/receivingParty.ts
@@ -0,0 +1,4 @@
+export class ReceivingParty{
+    id: number = 0;
+    label: string = "";
+}
